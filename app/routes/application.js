8a5276e13eb4f400d82302fe99e951b7d7e16dd5
PenelopeYueZhu <lxyzhu7@gmail.com>
2019-07-27 21:09:49 -0500

    fixed serilizers. Finished styling the top banner

--- app/routes/application.js
+++ app/routes/application.js
@@ -8,68 +8,28 @@ export default Route.extend({
   model(){
 
     var store = this.store;
-    return hash({
-      // Getting all the posts
-      post: this.get('ajax').request('/posts' ).catch( error => {
-              if( isAjaxError( error ) ) {
-                //handle ajax error here
-                return;
-              }
-            })
-            .then(
-              responsePost => {
-                store.pushPayload( 'post', responsePost );
-                store.findAll( 'post');
-            }),
 
-      // Getting all the users
-      user: this.get('ajax').request('/users' ).catch( error => {
-              if( isAjaxError( error ) ) {
-                //handle ajax error here
-                return;
-              }
-            })
-            .then(
-              responseUser => {
-                store.pushPayload( 'user', responseUser );
-                store.findAll( 'user');
-            }),
-
-      // Getting all the comments
-      comment: this.get('ajax').request('/comments' ).catch( error => {
-                if( isAjaxError( error ) ) {
-                  //handle ajax error here
-                  return;
-                }
-              })
-              .then(
-                responseComment => {
-                  store.pushPayload( 'comment', responseComment );
-                  store.findAll( 'comment');
-              })
-    })
-    .then ( function( ) {
-      store.findAll('post').then( posts => {
-        // Setting the relationship between post and user
-        posts.forEach( post => {
-          var userId = post.get('userId');
-          let user = store.peekRecord( 'user', userId );
-          post.set('user', user);
-        });
-        posts.save();
+    // Get all the user just to populate store 
+    store.findAll('user');
+    store.findAll('post').then( posts => {
+      // Setting the relationship between post and user
+      posts.forEach( post => {
+        var userId = post.get('userId');
+        let user = store.peekRecord( 'user', userId );
+        post.set('user', user);
       });
+      posts.save();
+    });
 
-      //this.store.findAll('post');
-      // Getting all the records of comments
-      store.findAll('comment').then( comments => {
-        // Setting the relationship between comment and post
-        comments.forEach( comment => {
-          var postId = comment.get( 'postId' );
-          let post = store.peekRecord( 'post', postId );
-          comment.set( 'post', post );
-        });
-        comments.save();
-      }); // end of find all commet
-    }); // end of then
+    // Getting all the records of comments
+    store.findAll('comment').then( comments => {
+      // Setting the relationship between comment and post
+      comments.forEach( comment => {
+        var postId = comment.get( 'postId' );
+        let post = store.peekRecord( 'post', postId );
+        comment.set( 'post', post );
+      });
+      comments.save();
+    }); // end of find all commet
   }
 });
---
+++ app/serializers/application.js
@@ -0,0 +1,10 @@
+/**
+ * File to ajust post data key to JSON standard
+ */
+
+import DS from 'ember-data';
+
+export default DS.JSONSerializer.extend( {
+
+
+} );
