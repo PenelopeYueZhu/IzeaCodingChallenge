f600bd6e5e64576c75b5ab02f2c059b85c7eb801
PenelopeYueZhu <lxyzhu7@gmail.com>
2019-07-29 13:29:53 -0500

    cleaned up codes

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,7 +3,3 @@ import DS from 'ember-data';
 import AjaxServiceSupport from 'ember-ajax/mixins/ajax-support';
 
 export default DS.RESTAdapter.extend(AjaxServiceSupport);
-// For testing with mirage
-//export default DS.RESTAdapter.extend({
-//  namespace: 'api'
-//});
